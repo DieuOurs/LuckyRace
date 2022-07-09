cefe4a46c517ec54e3a574a9e78f6113a2ab4787
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 11:38:19 +0400

    Initialization of Plugin

---
+++ src/main/java/fr/dieuours/luckyrace/LuckyRace.java
@@ -0,0 +1,21 @@
+package fr.dieuours.luckyrace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LuckyRace extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
