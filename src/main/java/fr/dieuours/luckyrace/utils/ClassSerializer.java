68d626515ee8d558483dd7ea9b983d56884d2365
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 14:19:00 +0400

    Add class ClassSerializer.java methods: - toString which serialize Object to String - deserialize which deserialize String to Object by passing Class extended ClassSerializer.java in parameter

---
+++ src/main/java/fr/dieuours/luckyrace/utils/ClassSerializer.java
@@ -0,0 +1,36 @@
+package fr.dieuours.luckyrace.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class ClassSerializer {
+
+    private static final Gson gson;
+
+    static {
+        gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .disableHtmlEscaping()
+                .create();
+    }
+
+    /**
+     * @param c
+     * @param json
+     * @return gson.fromJson(String, Class < ? extends ClassSerializer >);
+     * @see ClassSerializer
+     * @see Class
+     */
+    public static Object deserialize(Class<? extends ClassSerializer> c, String json) {
+        return gson.fromJson(json, c);
+    }
+
+    /**
+     * @return this.gson.toJson(this);
+     */
+    @Override
+    public String toString() {
+        return gson.toJson(this);
+    }
+}
