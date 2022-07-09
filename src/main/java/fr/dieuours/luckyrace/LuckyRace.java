3517c5eaf040485583b44d3b147744093588a43f
BuildTools <unconfigured@null.spigotmc.org>
2022-07-09 13:44:28 +0400

    Updates LuckyRace.java + instance + comments

--- src/main/java/fr/dieuours/luckyrace/LuckyRace.java
+++ src/main/java/fr/dieuours/luckyrace/LuckyRace.java
@@ -4,18 +4,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class LuckyRace extends JavaPlugin {
 
+    private static LuckyRace instance;
+
+    public static LuckyRace getInstance() {
+        return instance;
+    }
+
+    /**
+     * When plugin start, this method is called
+     *
+     * @see JavaPlugin
+     */
     @Override
     public void onLoad() {
         super.onLoad();
     }
 
+    /**
+     * When plugin stop, this method is called
+     *
+     * @see JavaPlugin
+     */
     @Override
     public void onDisable() {
         super.onDisable();
     }
 
+    /**
+     * When plugin is load, this method is called
+     *
+     * @see JavaPlugin
+     */
     @Override
     public void onEnable() {
         super.onEnable();
+        instance = this;
     }
 }
