1a0143b650d3c730ba4edf734aace349218e4337
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 14:51:55 +0400

    add game status

--- src/main/java/fr/dieuours/luckyrace/core/game/LuckyGame.java
+++ src/main/java/fr/dieuours/luckyrace/core/game/LuckyGame.java
@@ -5,21 +5,34 @@ import org.bukkit.Location;
 
 public class LuckyGame extends LuckyNewGame {
 
-    private Location waitingLobby;
+    private Location waitingLobby, lobbyWhenGameEnd;
     private FinalLine finalLine;
+    private Status status;
 
     /**
-     * @param waitingLobby Location
-     * @param finalLine    FinalLine
-     * @param luckyNewGame LuckyNewGame
+     * @param waitingLobby     Location
+     * @param luckyNewGame     LuckyNewGame
+     * @param lobbyWhenGameEnd Location
+     * @param finalLine        FinalLine
+     * @param status           Status
      * @see FinalLine
      * @see LuckyNewGame
      */
-    public LuckyGame(Location waitingLobby, LuckyNewGame luckyNewGame, FinalLine finalLine) {
+    public LuckyGame(Location waitingLobby, LuckyNewGame luckyNewGame, Location lobbyWhenGameEnd, FinalLine finalLine, Status status) {
         super(luckyNewGame.getUuid(), luckyNewGame.getMaxPlayers(), luckyNewGame.getMinPlayers(),
                 luckyNewGame.getPlayersSpawn(), luckyNewGame.getLuckyChests(), luckyNewGame.getLuckyPlayerInGames());
         this.waitingLobby = waitingLobby;
+        this.lobbyWhenGameEnd = lobbyWhenGameEnd;
         this.finalLine = finalLine;
+        this.status = status;
+    }
+
+    public Location getLobbyWhenGameEnd() {
+        return lobbyWhenGameEnd;
+    }
+
+    public void setLobbyWhenGameEnd(Location lobbyWhenGameEnd) {
+        this.lobbyWhenGameEnd = lobbyWhenGameEnd;
     }
 
     public Location getWaitingLobby() {
@@ -36,5 +49,17 @@ public class LuckyGame extends LuckyNewGame {
 
     public void setFinalLine(FinalLine finalLine) {
         this.finalLine = finalLine;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public enum Status {
+        WAITING,
+        LOBBY,
+        IN_GAME,
+        RESET,
+        ERROR,
     }
 }
