4b11fede8702eba6883ffe2bdeb85c0bb29e811d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 16:34:19 +0400

    fill case WAITING add case END

--- src/main/java/fr/dieuours/luckyrace/core/listeners/LuckyGameStatusListener.java
+++ src/main/java/fr/dieuours/luckyrace/core/listeners/LuckyGameStatusListener.java
@@ -3,6 +3,8 @@ package fr.dieuours.luckyrace.core.listeners;
 import fr.dieuours.luckyrace.LuckyRace;
 import fr.dieuours.luckyrace.core.events.LuckyGameStatusChangeEvent;
 import fr.dieuours.luckyrace.core.timers.LuckyLobbyGameTimer;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -21,9 +23,17 @@ public class LuckyGameStatusListener implements Listener {
     public void onLuckyGameStatusChangeEvent(LuckyGameStatusChangeEvent e) {
         switch (e.getStatus()) {
             case LOBBY:
+                e.getLuckyGame().sendMessageToEveryPlayerInGame("The lobby timer start...");
                 new LuckyLobbyGameTimer(e.getLuckyGame()).runTaskTimer(pl, 20L, 20L);
                 break;
             case WAITING:
+                e.getLuckyGame().sendMessageToEveryPlayerInGame("The game will be start when the size of minimum of players reach " + e.getLuckyGame().getMinPlayers());
+                break;
+            case END:
+                e.getLuckyGame().sendMessageToEveryPlayerInGame("The game will be ended");
+                e.getLuckyGame().getLuckyPlayerInGames().forEach(x -> Bukkit.getPlayer(x.getUuid()).setGameMode(GameMode.SPECTATOR));
+                break;
+            case IN_GAME:
                 break;
         }
     }
