f0865ebb855cbf138c788b5f8bce9cea3505600e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 16:36:14 +0400

    add methods + getLuckyGame() + getLuckyPlayerInGame()
    const call LuckyGameRemovePlayerEvent.java

--- src/main/java/fr/dieuours/luckyrace/core/events/LuckyPlayerLeaveGameEvent.java
+++ src/main/java/fr/dieuours/luckyrace/core/events/LuckyPlayerLeaveGameEvent.java
@@ -1,9 +1,11 @@
 package fr.dieuours.luckyrace.core.events;
 
 import fr.dieuours.luckyrace.LuckyRace;
+import fr.dieuours.luckyrace.core.exceptions.GameException;
 import fr.dieuours.luckyrace.core.exceptions.GameNotFoundException;
 import fr.dieuours.luckyrace.core.game.LuckyGame;
 import fr.dieuours.luckyrace.core.player.LuckyPlayerInGame;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
@@ -11,13 +13,15 @@ public class LuckyPlayerLeaveGameEvent extends Event {
 
     private static final LuckyRace pl = LuckyRace.getInstance();
     private static final HandlerList handlers = new HandlerList();
-    //TODO GETTER AND SETTER BUT THEY MIGHT BE USED
+    private final LuckyPlayerInGame luckyPlayerInGame;
     private final LuckyGame luckyGame;
 
-    public LuckyPlayerLeaveGameEvent(LuckyPlayerInGame luckyPlayerInGame) throws GameNotFoundException {
+    public LuckyPlayerLeaveGameEvent(LuckyPlayerInGame luckyPlayerInGame) throws GameException {
         if (pl.getLuckyGames().stream().noneMatch(x -> x.getUuid().equals(luckyPlayerInGame.getGameUUID())))
             throw new GameNotFoundException();
         this.luckyGame = pl.getLuckyGames().stream().filter(x -> x.getUuid().equals(luckyPlayerInGame.getGameUUID())).findFirst().get();
+        this.luckyPlayerInGame = luckyPlayerInGame;
+        Bukkit.getServer().getPluginManager().callEvent(new LuckyGameRemovePlayerEvent(this.luckyGame, luckyPlayerInGame));
     }
 
     //NOT DELETE
@@ -27,5 +31,13 @@ public class LuckyPlayerLeaveGameEvent extends Event {
 
     public HandlerList getHandlers() {
         return handlers;
+    }
+
+    public LuckyGame getLuckyGame() {
+        return luckyGame;
+    }
+
+    public LuckyPlayerInGame getLuckyPlayerInGame() {
+        return luckyPlayerInGame;
     }
 }
