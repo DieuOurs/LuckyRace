fc30fa4da3055f8c8e9bce4ecb17f370b8a3e7e2
BuildTools <unconfigured@null.spigotmc.org>
2022-07-10 14:52:13 +0400

    pre-add Listener

---
+++ src/main/java/fr/dieuours/luckyrace/core/listeners/LuckyPlayerGameListener.java
@@ -0,0 +1,19 @@
+package fr.dieuours.luckyrace.core.listeners;
+
+import fr.dieuours.luckyrace.core.events.LuckyPlayerJoinGameEvent;
+import fr.dieuours.luckyrace.core.events.LuckyPlayerLeaveGameEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class LuckyPlayerGameListener implements Listener {
+
+    @EventHandler
+    public void onLuckyPlayerJoinGameEvent(LuckyPlayerJoinGameEvent e) {
+
+    }
+
+    @EventHandler
+    public void onLuckyPlayerLeaveGameEvent(LuckyPlayerLeaveGameEvent e) {
+
+    }
+}
